30d6a77f1ce9a8eb612b0a33f9daee95aefe3c2d
mkearney <kearneymw@missouri.edu>
2020-03-22 14:18:39 -0500

    auto update

--- R/scrape.R
+++ R/scrape.R
@@ -1,9 +1,20 @@
 ##----------------------------------------------------------------------------##
-##                    package(s) & timestamp & state info                     ##
+##                          timestamp & state info                            ##
 ##----------------------------------------------------------------------------##
 
-## i like data.table right now
-library(data.table)
+## get fips info and create county name variable
+fips <- maps::county.fips[grepl("missouri,", maps::county.fips$polyname), ]
+fips$county <- sub("missouri,", "", fips$polyname)
+
+## capitalize
+m <- gregexpr("(^| )[a-z]", fips$county)
+regmatches(fips$county, m) <- dapr::lap(regmatches(fips$county, m), toupper)
+
+## add dot after st/ste
+fips$county <- sub("((?<=St)|(?<=Ste))(?= )", ".", fips$county, perl = TRUE)
+
+## convert to tibble
+fips <- tibble::as_tibble(fips[, c("fips", "county")])
 
 ## local file location
 dir <- "/home/kmw/rstats/covid-19-missouri"
@@ -13,7 +24,7 @@ time_stamp <- Sys.time()
 #time_stamp_utc <- as.POSIXct(format(time_stamp, tz = "UTC"), tz = "UTC")
 
 ## no fips code because i'm lazy and don't have them for counties
-state_info <- data.table(
+state_info <- tibble::tibble(
   state = "Missouri",
   state_abb = "MO"
 )
@@ -30,73 +41,64 @@ dats <- rvest::html_table(h, fill = TRUE)
 ##                                 data tables                                ##
 ##----------------------------------------------------------------------------##
 ## by county test results
-d1 <- as.data.table(dats[[1]])
-names(d1) <- c("county", "state_lab", "other_lab")
-d1[, state_lab := ifelse(is.na(state_lab), 0L, state_lab)]
-d1[, other_lab := ifelse(is.na(other_lab), 0L, other_lab)]
-d1[, .timestamp := time_stamp]
+d1 <- tibble::as_tibble(dats[[1]])
+while (ncol(d1) > 4) {
+  d1[[4]] <- d1[[4]] + d1[[5]]
+  d1[[5]] <- NULL
+}
+names(d1) <- c("county", "total", "state_lab", "other_lab")
+d1[["total"]] <- ifelse(is.na(d1[["total"]]), 0L, d1[["total"]])
+d1[["state_lab"]] <- ifelse(is.na(d1[["state_lab"]]), 0L, d1[["state_lab"]])
+d1[["other_lab"]] <- ifelse(is.na(d1[["other_lab"]]), 0L, d1[["other_lab"]])
+
+## add fips and timestamp info
+d1$county <- sub(" County", "", d1$county)
+d1 <- dplyr::left_join(d1, fips)
+d1$timestamp <- time_stamp
 
 ## by age
-d2 <- as.data.table(dats[[2]])
+d2 <- tibble::as_tibble(dats[[2]])
 names(d2) <- c("age_range", "cases")
-d2[, age_range := sub("Under 20", "0-20", age_range)]
-d2[, .timestamp := time_stamp]
+d2[["age_range"]] <- sub("Under 20", "0-20", d2[["age_range"]])
 
 ## by transmission type
-d3 <- as.data.table(dats[[3]])
+d3 <- tibble::as_tibble(dats[[3]])
 names(d3) <- c("transmission", "cases")
-d3[, transmission := tolower(transmission)]
-d3[, .timestamp := time_stamp]
+d3[["transmission"]] <- tolower(d3[["transmission"]])
 
-## by state (total)
-meta <- data.table(
-  total_cases = sum(c(d1[, state_lab], d1[, other_lab])),
-  state_labs = sum(d1[, state_lab]),
-  other_labs = sum(d1[, other_lab]),
-  age_70_plus = sum(d2[age_range=="70+", cases]),
-  age_under_70 = sum(d2[age_range!="70+", cases]),
-  travel_related = sum(d3[transmission == "travel", cases]),
-  contact_related = sum(d3[transmission == "contact", cases]),
-  unknown_related = sum(d3[transmission == "unknown", cases]),
-  time = time_stamp
+## state of missouri totals
+mo <- tibble::tibble(
+  total_cases = sum(d1[["total"]]),
+  state_labs = sum(d1[["state_lab"]]),
+  other_labs = sum(d1[["other_lab"]]),
 )
+ages <- paste0("ages_", sub("\\+", "_plus", sub("-", "_", d2[["age_range"]])))
+for (i in seq_along(ages)) {
+  mo[[ages[i]]] <- d2[i, "cases"][[1]]
+}
 
-## all data
-d <- list(
-  by_state = cbind(state_info, meta),
-  by_county = cbind(state_info, d1),
-  by_age_range = cbind(state_info, d2),
-  by_transmission = cbind(state_info, d3)
-)
+transmissions <- paste0("transmission_", janitor::make_clean_names(d3[["transmission"]]))
+for (i in seq_along(transmissions)) {
+  mo[[transmissions[i]]] <- d3[i, "cases"][[1]]
+}
+
+## add timestamp
+mo[[".timestamp"]] <- time_stamp
+
+## combine with state info
+mo <- cbind(state_info, mo)
 
 ##----------------------------------------------------------------------------##
 ##                                 write CSVs                                 ##
 ##----------------------------------------------------------------------------##
 
-d$by_state <- rbind(
-  d$by_state,
-  readr::read_csv(file.path(dir, "data", "mo-total.csv"))
-)
-d$by_county <- rbind(
-  d$by_county,
-  readr::read_csv(file.path(dir, "data", "mo-county.csv"))
-)
-d$by_age <- rbind(
-  d$by_age,
-  readr::read_csv(file.path(dir, "data", "mo-age.csv"))
-)
-d$by_transmission <- rbind(
-  d$by_transmission,
-  readr::read_csv(file.path(dir, "data", "mo-transmission.csv"))
-)
-
-readr::write_csv(d$by_state, file.path(dir, "data", "mo-total.csv"))
-readr::write_csv(d$by_county, file.path(dir, "data", "mo-county.csv"))
-readr::write_csv(d$by_age, file.path(dir, "data", "mo-age.csv"))
-readr::write_csv(d$by_transmission, file.path(dir, "data", "mo-transmission.csv"))
+mo <- rbind(mo, readr::read_csv(file.path(dir, "data", "mo-total.csv")))
+readr::write_csv(mo, file.path(dir, "data", "mo-total.csv"))
+d1 <- rbind(d1, readr::read_csv(file.path(dir, "data", "mo-county.csv")))
+readr::write_csv(d1, file.path(dir, "data", "mo-county.csv"))
 
 data_files <- file.path(dir, "data",
-  paste0("mo-", c("total", "county", "age", "transmission"), ".csv"))
+  paste0("mo-", c("total", "county"), ".csv"))
 system(paste("cd", dir, "&&", "git add", paste(data_files, collapse = " ")))
 system(paste("cd", dir, "&&", "git commit -m \"auto update\"", dir))
 system(paste("cd", dir, "&&", "git push"))
